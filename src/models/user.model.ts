266456821ba561bc9f2d530299c986b66b52aef1
sakthivel9963 <sakthivel9963@gmail.com>
2019-10-20 14:33:01 +0530

    created a sample model with not required password parameter

---
+++ src/models/user.model.ts
@@ -0,0 +1,46 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model({settings: {strict: false}})
+export class User extends Entity {
+  @property({
+    type: 'number',
+    id: true,
+    required: true,
+    generated: true,
+  })
+  id: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  username: string;
+
+  @property({
+    type: 'string',
+  })
+  password?: string;
+
+  @property({
+    type: 'date',
+    required: true,
+    default: $now,
+  })
+  createdAt: string;
+
+  // Define well-known properties here
+
+  // Indexer property to allow additional data
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  [prop: string]: any;
+
+  constructor(data?: Partial<User>) {
+    super(data);
+  }
+}
+
+export interface UserRelations {
+  // describe navigational properties here
+}
+
+export type UserWithRelations = User & UserRelations;
