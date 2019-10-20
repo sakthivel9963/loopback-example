d435926d6dfcd21d9d75c135c81157e1b884415a
sakthivel9963 <sakthivel9963@gmail.com>
2019-10-20 14:55:10 +0530

    added the database migration in the index.ts fiel

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ export {LoopbackExampleApplication};
 export async function main(options: ApplicationConfig = {}) {
   const app = new LoopbackExampleApplication(options);
   await app.boot();
+  await app.migrateSchema();
   await app.start();
 
   const url = app.restServer.url;
